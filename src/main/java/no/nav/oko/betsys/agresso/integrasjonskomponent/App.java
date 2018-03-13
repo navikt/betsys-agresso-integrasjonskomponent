3a4186dd2bcfefdfcbd915a721e3fd04f0e045cf
Eriksen, Helge <Helge.Eriksen@nav.no>
2018-03-13 10:14:34 +0100

    Initial commit with gitignore and pom

---
+++ src/main/java/no/nav/oko/betsys/agresso/integrasjonskomponent/App.java
@@ -0,0 +1,13 @@
+package no.nav.oko.betsys.agresso.integrasjonskomponent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
