db64e98cd1ad073f642deaf60311114aa0a5953f
Myhrstuen, Martin <Martin.Myhrstuen@nav.no>
2018-04-06 13:09:08 +0200

    Oppretter lazyTransactionManager i første omgang

--- src/main/java/no/nav/oko/betsys/agresso/integrasjonskomponent/config/JmsConfig.java
+++ src/main/java/no/nav/oko/betsys/agresso/integrasjonskomponent/config/JmsConfig.java
@@ -53,7 +53,8 @@ public class JmsConfig {
     public JmsConfiguration jmsConfiguration(ConnectionFactory connectionFactory) {
         JmsConfiguration jmsConfiguration = new JmsConfiguration();
         jmsConfiguration.setConnectionFactory(connectionFactory);
-//        jmsConfiguration.setTransacted(true);
+        jmsConfiguration.setTransacted(true);
+        jmsConfiguration.setLazyCreateTransactionManager(true);
         return jmsConfiguration;
     }
 
