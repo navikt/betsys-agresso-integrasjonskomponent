a995f9b54ffef3fe3c77008920ca216dc7bac7a1
Eriksen, Helge <Helge.Eriksen@nav.no>
2018-06-08 15:31:39 +0200

    endrer neste

--- src/test/java/no/nav/oko/betsys/agresso/integrasjonskomponent/IntegrasjonskomponentITest.java
+++ src/test/java/no/nav/oko/betsys/agresso/integrasjonskomponent/IntegrasjonskomponentITest.java
@@ -80,7 +80,7 @@ public class IntegrasjonskomponentITest
         Files.createDirectories(Paths.get(mainPath,filstiTilBetsysInn));
         SFTPServerConfig serverConfig = new SFTPServerConfig();
         agressoServer = serverConfig.configure("127.0.0.4",2222, "Agresso");
-        betsysServer = serverConfig.configure("127.0.0.3",2222, "Betsys");
+        betsysServer = serverConfig.configure("127.0.0.5",2222, "Betsys");
         agressoServer.start();
         betsysServer.start();
     }
